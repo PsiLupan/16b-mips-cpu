46c01a46176f9efa45a5b8c0e876f6b116ac7fd4
PsiLupan <wcarter312@gmail.com>
2016-04-07 23:46:45 -0500

    Fixed defintions for when the functions are actually used.

--- Memory.cpp
+++ Memory.cpp
@@ -2,12 +2,12 @@
 
 /* Returns the content of Memory[addr / 2], since our addresses are a DWORD, instead of referencing a single WORD.
 */
-static uint16_t getContent(uint16_t addr){
+uint16_t Memory::getContent(uint16_t addr){
 	return Memory::data[addr / 2];
 }
 
 /* Stores the data at Memory[addr / 2].
 */
-static void storeContent(uint16_t addr, uint16_t data){
+void Memory::storeContent(uint16_t addr, uint16_t data){
 	Memory::data[addr / 2] = data;
 }
--- Memory.h
+++ Memory.h
@@ -5,6 +5,6 @@ namespace Memory {
 	static std::array<uint16_t, 64> instr; //256 bytes of instruction memory
 	static std::array<uint16_t, 64> data; //256 bytes of data memory
 
-	static uint16_t getContent(uint16_t addr);
-	static void storeContent(uint16_t addr, uint16_t data);
+	uint16_t getContent(uint16_t addr);
+	void storeContent(uint16_t addr, uint16_t data);
 }
